a284547edecb7a7ee1b9c6021cfc1b9cb2fd532f
Evan Hahn <me@evanhahn.com>
2018-05-15 20:38:21 -0500

    Remove Spectra from regular dependencies

--- src/convert.js
+++ src/convert.js
@@ -1,5 +1,3 @@
-const spectra = require('spectra')
-
 module.exports = function convert (canvas) {
   const {width, height} = canvas
   const ctx = canvas.getContext('2d')
@@ -14,7 +12,7 @@ module.exports = function convert (canvas) {
     for (let x = 0; x < width; x++) {
       const index = ((y * width) + x) * 4
       const [r, g, b] = [data[index], data[index + 1], data[index + 2]]
-      const bgcolor = spectra({ r, g, b }).hex()
+      const bgcolor = hex(r, g, b)
       row.push(`<td bgcolor=${bgcolor} />`)
     }
     row.push('</tr>')
@@ -24,4 +22,12 @@ module.exports = function convert (canvas) {
   result.push('</table>')
 
   return result.join('')
+}
+
+function hex (...rgbArray) {
+  return rgbArray.reduce((result, color) => {
+    let hexed = color.toString(16)
+    if (hexed.length === 1) { hexed = '0' + hexed }
+    return result + hexed
+  }, '#')
 }
