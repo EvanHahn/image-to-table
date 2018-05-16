17e707763d28047daf15a7a65768bbddc04c80e0
Evan Hahn <me@evanhahn.com>
2018-05-15 20:22:55 -0500

    Initial commit

---
+++ src/convert.js
@@ -0,0 +1,27 @@
+const spectra = require('spectra')
+
+module.exports = function convert (canvas) {
+  const {width, height} = canvas
+  const ctx = canvas.getContext('2d')
+  const {data} = ctx.getImageData(0, 0, width, height)
+
+  const result = [
+    '<table border=0 cellspacing=0 cellpadding=0>'
+  ]
+
+  for (let y = 0; y < height; y++) {
+    const row = ['<tr>']
+    for (let x = 0; x < width; x++) {
+      const index = ((y * width) + x) * 4
+      const [r, g, b] = [data[index], data[index + 1], data[index + 2]]
+      const bgcolor = spectra({ r, g, b }).hex()
+      row.push(`<td bgcolor=${bgcolor}></td>`)
+    }
+    row.push('</tr>')
+    result.push(...row)
+  }
+
+  result.push('</table>')
+
+  return result.join('')
+}
---
+++ test/convert.test.js
@@ -0,0 +1,48 @@
+const convert = require('../src/convert')
+const path = require('path')
+const { createCanvas, loadImage } = require('canvas')
+const cheerio = require('cheerio')
+const spectra = require('spectra')
+
+const SAMPLE_IMAGE_PATH = path.join(__dirname, 'sample-image.png')
+
+test('converts an image', async () => {
+  const canvas = createCanvas(3, 3)
+  const ctx = canvas.getContext('2d')
+  const image = await loadImage(SAMPLE_IMAGE_PATH)
+  ctx.drawImage(image, 0, 0)
+
+  const result = convert(canvas)
+
+  const $ = cheerio.load(result)
+
+  const $table = $('table')
+  expect($table).toHaveLength(1)
+  expect($table.attr('border')).toEqual('0')
+  expect($table.attr('cellspacing')).toEqual('0')
+  expect($table.attr('cellpadding')).toEqual('0')
+
+  const $tr = $('table tr')
+  expect($tr).toHaveLength(3)
+
+  const $td = $('table td')
+  expect($td).toHaveLength(9)
+
+  console.log(result)
+
+  ;[
+    'black',
+    'white',
+    '#fb0207',
+    '#0000ff',
+    '#fb02ff',
+    '#21ff06',
+    '#ffff0a',
+    '#fd8008',
+    '#800080'
+  ].forEach((color, index) => {
+    const expected = spectra(color)
+    const actual = spectra($td.eq(index).attr('bgcolor'))
+    expect(actual.equals(expected)).toBeTruthy()
+  })
+})
